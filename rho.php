0976d59b0d75886980fc1ee12eaaa6eae01eb102
blanquerna <39979950+blanquerna@users.noreply.github.com>
2023-02-21 16:31:41 +0100

    Add files via upload

---
+++ rho.php
@@ -0,0 +1,22 @@
+<?php
+// Rho Theme extension, https://github.com/
+
+class YellowRho {
+    const VERSION = "0.8.14";
+    public $yellow;         // access to API
+    
+    // Handle initialisation
+    public function onLoad($yellow) {
+        $this->yellow = $yellow;
+    }
+    
+    // Handle update
+    public function onUpdate($action) {
+        $fileName = $this->yellow->system->get("coreExtensionDirectory").$this->yellow->system->get("coreSystemFile");
+        if ($action=="install") {
+            $this->yellow->system->save($fileName, array("theme" => "rho"));
+        } elseif ($action=="uninstall" && $this->yellow->system->get("theme")=="rho") {
+            $this->yellow->system->save($fileName, array("theme" => $this->yellow->system->getDifferent("theme")));
+        }
+    }
+}
